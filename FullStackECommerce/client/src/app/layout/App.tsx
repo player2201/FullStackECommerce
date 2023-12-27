10a2eb9c2a79586c07afdde75f7b42f8217c2a1b
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2023-12-28 00:55:47 +0200

    Front End added light/dark mode.

---
+++ FullStackECommerce/client/src/app/layout/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container>
+          <Catalog />
+        </Container>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
