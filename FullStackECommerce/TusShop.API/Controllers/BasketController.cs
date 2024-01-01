4862215360900afe20e2f8e50a482764bc47aba4
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2024-01-01 20:28:17 +0200

    Added Redux for state management instead.

--- FullStackECommerce/TusShop.API/Controllers/BasketController.cs
+++ FullStackECommerce/TusShop.API/Controllers/BasketController.cs
@@ -33,7 +33,7 @@ namespace TusShop.API.Controllers
                 basket = CreateBasket();
             var product = await _context.Products.FindAsync(productId);
             if (product == null)
-                return NotFound();
+                return BadRequest(new ProblemDetails { Title = "Product Not Found" });
             basket.AddItem(product, quantity);
             var result = await _context.SaveChangesAsync() > 0;
             if (result)
